cb27fb63a4c42cf247d1801f3b060d14f8306728
Mouhamed Sylla <syllamouhamed99@gmail.com>
2025-09-14 20:17:36 +0000

    adding function to write file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from functions.config import MAX_CHARS
 
 def get_files_info(working_directory, directory="."):
     abs_working_directory = os.path.abspath(working_directory)
@@ -30,3 +31,45 @@ def formatting_output(listdir, target_path):
         lines.append(f' - {file}: file_size={os.path.getsize(full_path)} is_dir={os.path.isdir(full_path)}')
 
     return "\n".join(lines)
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    target_file_path = os.path.join(abs_working_directory, file_path)
+
+    if not os.path.isfile(target_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    if not os.path.abspath(target_file_path).startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        with open(target_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if os.path.getsize(target_file_path) > MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+
+            return file_content_string
+    except PermissionError:
+        print("Error: no permission for reading file")
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)
+    target_file_path = os.path.join(abs_working_directory, file_path)
+
+    if not os.path.abspath(target_file_path).startswith(abs_working_directory):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        with open(target_file_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        
+    except Exception as e:
+        print("Error: can't write file")
--- tests.py
+++ tests.py
@@ -1,13 +1,31 @@
 from functions import get_files_info as functions  # importe ta fonction
 
+# def run_tests():
+#     print(functions.get_files_info("calculator", "."))
+#     print()
+#     print(functions.get_files_info("calculator", "pkg"))
+#     print()
+#     print(functions.get_files_info("calculator", "/bin"))
+#     print()
+#     print(functions.get_files_info("calculator", "../"))
+
+
+# def run_tests():
+#     print(functions.get_file_content("calculator", "main.py"))
+#     print()
+#     print(functions.get_file_content("calculator", "pkg/calculator.py"))
+#     print()
+#     print(functions.get_file_content("calculator", "/bin/cat"))
+#     print()
+#     print(functions.get_file_content("calculator", "pkg/does_not_exist.py"))
+
+
 def run_tests():
-    print(functions.get_files_info("calculator", "."))
+    print(functions.write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
     print()
-    print(functions.get_files_info("calculator", "pkg"))
+    print(functions.write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
     print()
-    print(functions.get_files_info("calculator", "/bin"))
-    print()
-    print(functions.get_files_info("calculator", "../"))
+    print(functions.write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     run_tests()
